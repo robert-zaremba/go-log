e8d59c6a80d0ee96b0f9f74ad79b1c6c08e8c02e
Robert Zaremba <robert.zaremba@zoho.com>
2013-03-19 22:39:01 +0100

    Better prefix name in example

--- example/example.go
+++ example/example.go
@@ -9,10 +9,10 @@ var Log = log.New()
 
 func main() {
 	//Log.AddOutput(os.Stdout, clog.LevelWarning)
-	Log.AddOutput(os.Stderr, log.Levels.Info, log.StdFormatter{"root ", log.Lshortfile, false})
-	Log.AddOutput(os.Stderr, log.Levels.Info, log.StdFormatter{"with time", log.Lshortfile | log.Ldate | log.Ltime, false})
+	Log.AddOutput(os.Stderr, log.Levels.Info, log.StdFormatter{"[root]", log.Lshortfile, false})
+	Log.AddOutput(os.Stderr, log.Levels.Info, log.StdFormatter{"[with time]", log.Lshortfile | log.Ldate | log.Ltime, false})
 	x := 1.23
-	Log.Info("x is %v", x)
+	Log.Error("x is %v", x)
 	Log.Debug("This won't be printed")
 
 	Log = log.NewStd(os.Stderr, log.Levels.Debug, "", log.Ldate|log.Lmicroseconds, true)
