9cc6cc37cf7077edbd5dd445ca53d03348ef63ef
Robert Zaremba <robert.zaremba@zoho.com>
2013-03-19 20:37:34 +0100

    copied flags definition from std log package

--- example/example.go
+++ example/example.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/scale-it/go-log"
-	log2 "log"
 	"os"
 )
 
@@ -10,13 +9,13 @@ var Log = log.New()
 
 func main() {
 	//Log.AddOutput(os.Stdout, clog.LevelWarning)
-	Log.AddOutput(os.Stderr, log.Levels.Info, log.StdFormatter{"root ", log2.Lshortfile, false})
-	Log.AddOutput(os.Stderr, log.Levels.Info, log.StdFormatter{"with time", log2.Lshortfile | log2.Ldate | log2.Ltime, false})
+	Log.AddOutput(os.Stderr, log.Levels.Info, log.StdFormatter{"root ", log.Lshortfile, false})
+	Log.AddOutput(os.Stderr, log.Levels.Info, log.StdFormatter{"with time", log.Lshortfile | log.Ldate | log.Ltime, false})
 	x := 1.23
 	Log.Info("x is %v", x)
 	Log.Debug("This won't be printed")
 
-	Log = log.NewStd(os.Stderr, log.Levels.Debug, "", log2.Ldate|log2.Lmicroseconds, true)
+	Log = log.NewStd(os.Stderr, log.Levels.Debug, "", log.Ldate|log.Lmicroseconds, true)
 	Log.Debug("some message")
 	Log.Info("some message")
 	Log.Warning("some message")
