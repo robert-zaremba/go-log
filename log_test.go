a4da13f8ce99db2ff5db0967ca774f1ff3701901
Robert Zaremba <robert.zaremba@zoho.com>
2013-05-17 13:15:44 +0200

    Added Example test

---
+++ log_test.go
@@ -0,0 +1,19 @@
+package log_test
+
+import (
+	"github.com/scale-it/go-log"
+	"os"
+)
+
+func Example() {
+	var Log *log.Logger = log.New()
+	formatter := log.StdFormatter{"[root]", log.Lshortfile, false}
+	Log.AddHandler(os.Stdout, log.Levels.Warning, formatter)
+	pi := 3.14
+	Log.Infof("Pi is %v", pi)
+	Log.Warningf("Pi is %v", pi)
+	Log.Error("Pi is", pi)
+	// Output:
+	// WARN  log_test.go:14 [root] Pi is 3.14
+	// ERROR log_test.go:15 [root] Pi is 3.14
+}
